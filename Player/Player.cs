5785ff41c9b9ea7a5ca640aabaa7366b8346253e
DO-III <davidiii@uw.edu>
2022-07-13 16:45:43 -0700

    Basic movement animation

--- Player/Player.cs
+++ Player/Player.cs
@@ -1,21 +1,45 @@
 using Godot;
 using System;
 
-public class Player : KinematicBody2D //Extends Area2D
+public class Player : Area2D //Extends Area2D
 {
 	// Declare member variables here. Examples:
 	// private int a = 2;
 	// private string b = "text";
 	
-	private const float SPEED = 100;
+	[Export]
+	private float SPEED = 200; //Move speed.
+	
+	private Vector2 screenSize;
 	private Vector2 velocity = Vector2.Zero;
+	
+	private Sprite mySprite;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		screenSize = GetViewportRect().Size;
+		mySprite = GetNode("Sprite") as Sprite;
 		
 	}
 	
+	//For not-so-time-sensitive general behavior.
+	//Handles animation at the moment.
+	public override void _Process(float delta) {
+		//Change frame according to movement.
+		if (velocity.x < 0) {
+			mySprite.SetFrame(0);
+		} else if (velocity.x > 0) {
+			mySprite.SetFrame(2);
+		} else {
+			mySprite.SetFrame(1);
+		}
+		
+	}
+	
+	
+	//For time-sensitive physics calculations.
+	//We do this stuff here as this function makes sure everything fires ASAP.
 	public override void _PhysicsProcess(float delta) {
 		
 		velocity.x = 0;
@@ -34,14 +58,15 @@ public class Player : KinematicBody2D //Extends Area2D
 		}
 		
 		velocity = velocity.Normalized() * SPEED;
-		velocity = MoveAndSlide(velocity);
+		
+		//Ensure we don't actually leave the screen.
+		
+		Position += velocity * delta;
+		Position = new Vector2(
+			x: Mathf.Clamp(Position.x, 0, screenSize.x),
+			y: Mathf.Clamp(Position.y, 0, screenSize.y)
+		);
 		
 		
 	}
-
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
 }
